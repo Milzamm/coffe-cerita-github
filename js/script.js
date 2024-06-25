0cef5a9e474389064e47c87b619964ca3a7c58d8
Milzamm <rafimilzam@gmail.com>
2024-06-25 13:29:03 +0700

    Coffe cerita

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//togle class active
+const navbarNav= document.querySelector
+('.navbar-nav');
+//ketika menu di klik
+document.querySelector('#hamburger-menu').
+onclick =()=>{
+navbarNav.classList.toggle('active');
+};
+
+//klik luar side bar keluar nav
+
+const hamburger=document.querySelector
+('#hamburger-menu');
+
+document.addEventListener('click',function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    navbarNav.classList.remove('active');
+    }  
+});
